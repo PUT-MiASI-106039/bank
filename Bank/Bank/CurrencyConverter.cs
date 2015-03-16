fb196a909f394e856fb00d1b81b4cb4d59c80307
ramzes1992 <adrian.ramzes@gmail.com>
2015-03-17 00:45:59 +0100

    Added CurrencyConverter

---
+++ Bank/Bank/CurrencyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public static class CurrencyConverter
+    {
+        public static Dictionary<Currency, double> BuyPrices = new Dictionary<Currency,double>();
+
+        public static Dictionary<Currency, double> SellPrices = new Dictionary<Currency, double>();
+
+        public static float Convert(Currency from, Currency to, double amount) 
+        {
+            return 0;
+        }
+
+    }
+}
