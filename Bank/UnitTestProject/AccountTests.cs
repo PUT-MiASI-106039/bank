d366f07550bcd7b6febc322a7625b5b8174b9656
Szymon <szmiekus@gmail.com>
2015-03-17 12:31:41 +0100

    Testy dla account

---
+++ Bank/UnitTestProject/AccountTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Bank.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void IncomeTest()
+        {
+            Account account = new Account();
+            account.State = 500;
+
+            account.Income(50);
+
+            Assert.AreEqual(550, account.State);
+        }
+
+        [TestMethod()]
+        public void ExcomeTest()
+        {
+            Account account = new Account();
+            account.State = 500;
+
+            account.Excome(50);
+
+            Assert.AreEqual(450, account.State);
+        }
+    }
+}
