6a6710fa20ef66b0f5a875fb345bc0183b5c1b96
ramzes1992 <adrian.ramzes@gmail.com>
2015-03-17 12:25:30 +0100

    Added ConvertTest

--- Bank/Bank/CurrencyConverter.cs
+++ Bank/Bank/CurrencyConverter.cs
@@ -8,12 +8,22 @@ namespace Bank
 {
     public static class CurrencyConverter
     {
-        public static Dictionary<Currency, double> BuyPrices = new Dictionary<Currency,double>();
+        //public static Currency MainCurrency = Currency.PLN;
 
-        public static Dictionary<Currency, double> SellPrices = new Dictionary<Currency, double>();
+        public static Dictionary<Currency, double> Prices = new Dictionary<Currency, double>();
 
-        public static float Convert(Currency from, Currency to, double amount) 
+        //public static Dictionary<Currency, double> SellPrices = new Dictionary<Currency, double>();
+
+        public static double Convert(Currency from, Currency to, double amount)
         {
+            if (Prices.ContainsKey(from)
+              && Prices.ContainsKey(to))
+            {
+                double fromMianCurrencyValue = Prices[from] * amount;
+
+                return fromMianCurrencyValue / Prices[to];
+            }
+
             return 0;
         }
 
