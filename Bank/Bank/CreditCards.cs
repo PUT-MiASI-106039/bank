5118611806765ddba0e1f3b60e6d3dfd8f5f29da
Szymon <szmiekus@gmail.com>
2015-03-17 10:46:20 +0100

    Wydmuszki + implementacja brakujących klas

---
+++ Bank/Bank/CreditCards.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class CreditCards
+    {
+        private double _limit;
+
+        private Account account;
+
+        public double Limit
+        {
+            get
+            {
+                return this._limit;
+            }
+
+            set
+            {
+                this._limit = value;
+            }
+        }
+
+        public CreditCards(Account account)
+        {
+            this.account = account;
+        }
+
+        public void Income(double amount)
+        {
+
+            account.State += amount;
+        }
+
+        public void Excome(double amount)
+        {
+
+            account.State -= amount;
+        }
+
+    }
+}
---
+++ Bank/Bank/Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public static class Operation
+    {
+        public static void Transfer(Account source, Account destiny, double amount)
+        {
+            if(amount > 0){
+
+                source.State -= amount;
+                destiny.State += amount;
+            }
+        }
+    }
+}
