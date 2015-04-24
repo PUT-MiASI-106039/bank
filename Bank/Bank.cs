3da723d442653447168487504b86777e2ccebf6a
Szymon <szmiekus@gmail.com>
2015-04-24 09:02:46 +0200

    Add Mediator Pattern

--- Bank/Bank.cs
+++ Bank/Bank.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bank.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,16 @@ namespace Bank
 {
     public class Bank
     {
+        private IMediator mediator;
+        private String id;
         public List<Account> Accounts { get; set; }
+
+        public Bank() { }
+
+        public Bank(String id) { 
+            
+            this.id = id; 
+        }
 
         public List<Person> GetClients()
         {
@@ -19,5 +29,23 @@ namespace Bank
 
             return new List<Person>();
         }
+
+        #region Mediator pattern
+
+        public void RegisterMediator(IMediator mediator) { this.mediator = mediator; }
+        public String getId() { return id; }
+
+        public void SendMoney(String id, int amount)
+        {
+            Console.WriteLine("Przesyłanie pieniędzy od " + this.id + " do " + id + ": " + amount);
+            mediator.SendMoney(id, amount); // Rzeczywista komunikacja odbywa się za pośrednictwem mediatora!!!
+        }
+
+        public void ReciveMoeny(int amount)
+        {
+            Console.WriteLine("Pieniądze odebranane przez " + id + ": " + amount);
+        }
+
+        #endregion
     }
 }
